de35f619cea6fe70f84501c00a9746f3d7fb777c
Nook Pawanrat Santiyanon <115173687+NookPawanrat@users.noreply.github.com>
2024-08-29 14:03:10 +0300

    Add CI and CD

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,8 +10,9 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) 
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -21,7 +22,7 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
+    validators = [
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
